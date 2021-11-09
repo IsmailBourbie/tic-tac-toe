ea3e78c12c000d962e6820c692d08ddde71c2391
Ismail Bourbie <bourbieismail@gmail.com>
2021-11-09 11:07:12 +0100

    added animate-css lib, start working with js

---
+++ v2/js/app.js
@@ -0,0 +1,12 @@
+// Change vs-player Start
+let vsComputer = document.getElementById('vs-computer'),
+    vsFriend = document.getElementById('vs-friend');
+
+vsComputer.addEventListener('click', function() {
+    alert('computer');
+});
+
+vsFriend.addEventListener('click', function() {
+    alert('Friend');
+});
+// Change vs-player End
